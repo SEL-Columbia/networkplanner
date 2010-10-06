a8f273b76368c44e0a2aa8660d98487aadbfe344
Roy Hyunjin Han <support@invisibleroads.com>
2010-10-05 21:21:40 -0400

    Missed one

--- deployment/cluster-queue-reset.sh
+++ deployment/cluster-queue-reset.sh
@@ -3,7 +3,7 @@ if [ ! -f .production.cfg ]; then
     echo "Please run this script in a directory containing .production.cfg"
 fi
 # If rabbitMQ has been reset,
-if [ "`rabbitmqctl list_users|grep guest`" = "guest" ]; then
+if [ "`/usr/sbin/rabbitmqctl list_users|grep guest`" = "guest" ]; then
     # Load credentials
     rabbitCredentials=`/bin/env python -c "import ConfigParser; c = ConfigParser.ConfigParser(); c.read('.production.cfg'); print c.get('amqp', 'username').strip() + ' ' + c.get('amqp', 'password').strip()"`
     rabbitUsername=${rabbitCredentials% *}
