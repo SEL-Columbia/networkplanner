163cdca6b011bd57772a36295026620e0c88ee6a
Chris Natali <chris.natali@gmail.com>
2012-05-16 18:30:20 -0400

    modified NP dependencies for ubuntu and added nginx config, 404 and 500 pages

--- deployment/dependencies-setup-ubuntu.sh
+++ deployment/dependencies-setup-ubuntu.sh
@@ -1,5 +1,7 @@
-apt-get install python-setuptools python-setuptools-devel gdal gdal-python python-matplotlib zlib proj python-sphinx postgresql-server python-psycopg2 rabbitmq-server
-easy_install -U shapely geojson amqplib sqlalchemy psycopg2 pylons recaptcha-client python-cjson scipy
+apt-get install python-setuptools libgdal-dev proj postgresql rabbitmq-server python-pip zlibc python-virtualenv python-dev python-numpy python-matplotlib python-gdal python-scipy
+easy_install -U shapely geojson amqplib sqlalchemy psycopg2 pylons recaptcha-client python-cjson scipy 
+# apt-get install python-setuptools python-setuptools-devel gdal gdal-python python-matplotlib zlib proj python-sphinx postgresql-server python-psycopg2 rabbitmq-server
+# easy_install -U shapely geojson amqplib sqlalchemy psycopg2 pylons recaptcha-client python-cjson scipy
 # updatedb
 # LIBPROJ_PATH=`locate libproj | grep so.0$`
 # LIBPROJ_PATH_LENGTH=${#LIBPROJ_PATH}
