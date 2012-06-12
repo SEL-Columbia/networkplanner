c414f6161198799cb17599c1654944f355dc951b
Chris Natali <chris.natali@gmail.com>
2012-06-11 21:15:47 -0400

    upped version#

--- np/config/parameter.py
+++ np/config/parameter.py
@@ -1,6 +1,6 @@
 # Set site parameters
 SITE_NAME = 'Network Planner'
-SITE_VERSION = '0.9.1'
+SITE_VERSION = '0.9.2'
 SITE_DESCRIPTION = 'Network Planning for Rural Electrification'
 SITE_KEYWORDS = 'Network planning, rural electrification'
 # Set person parameters
