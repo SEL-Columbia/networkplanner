15381dd3ed2abc0961e0d1afa02142239abb5e49
Chris Natali <chris.natali@gmail.com>
2012-07-16 18:07:31 -0400

    modified deployment scripts to use environment based command calls and sudo when needed.  removed pyyaml as a dependency (fabfile is now in a separate devops repo)

--- deployment/cluster-queue-reset.sh
+++ deployment/cluster-queue-reset.sh
@@ -1,19 +1,19 @@
-#!/bin/env bash
+#!/bin/bash
 if [ ! -f .production.cfg ]; then
     echo "Please run this script in a directory containing .production.cfg"
 fi
 # If rabbitMQ has been reset,
-if [ "`/usr/sbin/rabbitmqctl list_users|grep guest`" = "guest" ]; then
+if sudo rabbitmqctl list_users | grep '^guest'; then
     # Load credentials
-    rabbitCredentials=`/bin/env python -c "import ConfigParser; c = ConfigParser.ConfigParser(); c.read('.production.cfg'); print c.get('amqp', 'username').strip() + ' ' + c.get('amqp', 'password').strip()"`
+    rabbitCredentials=`python -c "import ConfigParser; c = ConfigParser.ConfigParser(); c.read('.production.cfg'); print c.get('amqp', 'username').strip() + ' ' + c.get('amqp', 'password').strip()"`
     rabbitUsername=${rabbitCredentials% *}
     rabbitPassword=${rabbitCredentials#* }
     # Initialize rabbitMQ
-    /usr/sbin/rabbitmqctl stop_app
-    /usr/sbin/rabbitmqctl reset
-    /usr/sbin/rabbitmqctl start_app
-    /usr/sbin/rabbitmqctl delete_user guest
-    /usr/sbin/rabbitmqctl add_user $rabbitUsername $rabbitPassword
-    /usr/sbin/rabbitmqctl set_permissions $rabbitUsername '.*' '.*' '.*'
-    /bin/env python utilities/requeue.py -c production.ini
+    sudo rabbitmqctl stop_app
+    sudo rabbitmqctl reset
+    sudo rabbitmqctl start_app
+    sudo rabbitmqctl delete_user guest
+    sudo rabbitmqctl add_user $rabbitUsername $rabbitPassword
+    sudo rabbitmqctl set_permissions $rabbitUsername '.*' '.*' '.*'
+    python utilities/requeue.py -c production.ini
 fi
