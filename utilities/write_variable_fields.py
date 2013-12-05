509ed5e81b25e967ca436f5d80922b5d469c793c
chrisnatali <chris.natali@gmail.com>
2013-12-05 11:41:40 -0500

    script to output relevant fields for model vars in csv form

---
+++ utilities/write_variable_fields.py
@@ -0,0 +1,33 @@
+import sys, os
+
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL)
+
+basePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(basePath)
+
+from np.lib import metric
+from np.lib import variable_store as VS
+from np import util
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) < 3):
+        sys.stderr.write("example usage:  python write_variable_fields.py model variable\n")
+        sys.exit()
+
+    # setup model
+    model = sys.argv[1]
+    variable = sys.argv[2] 
+
+    mvModel = metric.getModel(model)
+    modelVar = util.getSubModuleFromString(mvModel, variable)
+    variableSet = VS.getRelatedVariables(modelVar)
+
+    sys.stdout.write("class,alias,section,option,units,default\n")
+    for var in variableSet:
+        clazz = VS.getClassname(var)
+        alias = VS.getAlias(var)
+        sys.stdout.write("%s,%s,%s,%s,%s,%s\n" % (clazz, alias, var.section, 
+                         var.option, var.units, var.default))
+        
