e12df518fa065342c3692750d8a91f9134575122
chrisnatali <chris.natali@gmail.com>
2015-04-17 14:35:21 -0400

    added 'np-metrics' packaging for metrics computation only setup

---
+++ setup-metrics.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+"""
+setup file to be used for setting up metrics calculation module
+only (i.e. no spatial/network libraries)
+
+This can be used for running standalone metrics calc script or
+for use in other libraries that only need metrics calculations
+"""
+
+with open('requirements-metrics.txt') as f:
+    required = list(f.read().splitlines())
+
+setup(
+    name='np-metrics',
+    version='0.9.7',
+    description='Network planning metrics calculations',
+    url='http://networkplanner.modilabs.org',
+    install_requires=required,
+    packages=find_packages()
+    )
